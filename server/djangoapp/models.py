40ff72334b90f83beb9b63ec880641db7f8fb292
Libertocrat <libertocrat@proton.me>
2024-06-26 16:30:20 -0400

    Add: CarModel and CarMake Django Models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,14 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=128)
+    description = models.TextField()
+    country = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +31,40 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, null=False)
+    name = models.CharField(null=False, max_length=128)
+
+    CAR_TYPE_CHOICES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        ('TRUCK', 'Truck'),
+        ('CONVERTIBLE', 'Convertible')
+    ]
+    car_type = models.CharField(max_length=64, choices=CAR_TYPE_CHOICES, default = 'SEDAN')
+
+    year = models.IntegerField(null=False, default=2024,
+            validators=[
+                    MinValueValidator(2015),
+                    MaxValueValidator(2024) 
+                ]
+            )
+
+    MOTOR_TYPE_CHOICES = [
+        ('GAS', 'Gasoline'),
+        ('DIESEL', 'Diesel'),
+        ('ELECTRIC', 'Electric')
+    ]
+    motor_type = models.CharField(max_length=64, choices=MOTOR_TYPE_CHOICES, default = 'GAS')
+
+    # Extended car model description
+    description = models.TextField()
+
+    # True if the car model is still in production, False if no brand-new models ara available from the maker
+    in_production = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.make}-{self.name} {self.year}"
+
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,6 +15,9 @@ urlpatterns = [
     # Path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+    # Path for get_car
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    
     # path for dealer reviews view
 
     # path for add a review view
